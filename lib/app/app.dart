c14abec0970e9ae5d82030f7e56c8cc97c6523a7
Ahmed Elnemr <aelnemr711@gmail.com>
2024-02-02 23:24:47 +0200

    First Commit

---
+++ lib/app/app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/core/utils/app_assets.dart';
+import 'package:todo_app/core/utils/app_colors.dart';
+import 'package:todo_app/core/utils/app_strings.dart';
+
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppStrings.appName,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: AppColors.primary,
+        body: Center(
+            child: Image.asset(
+          AppImages.on1,
+        )),
+      ),
+    );
+  }
+}
---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,9 @@
+class AppImages {
+  static const String basedPath = 'assets/images/';
+
+  static const String logo = '${basedPath}app_logo.png';
+  static const String on1 = '${basedPath}on1.png';
+  static const String on2 = '${basedPath}on2.png';
+  static const String on3 = '${basedPath}on3.png';
+  static const String noTasks = '${basedPath}no_tasks.png';
+}
